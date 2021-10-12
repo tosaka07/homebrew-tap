4bed48c4fe12859da580f8f9db203f927b4d1420
tosaka07 <tosakaup@gmail.com>
2021-10-12 12:07:11 +0900

    Update

--- changeloggen.rb
+++ changeloggen.rb
@@ -1,8 +1,8 @@
 class Changeloggen < Formula
   desc "A command-line tool written in Swift that generates changelog"
   homepage "https://github.com/tosaka07/ChangelogGen"
-  url "https://github.com/tosaka07/ChangelogGen/archive/0.0.3.tar.gz"
-  sha256 "a1fd8aaa047783918286c0496816478b22d52a0f8124209c722b77478f0e5f54"
+  url "https://github.com/tosaka07/ChangelogGen/archive/0.0.4.tar.gz"
+  sha256 "fbac8045e05439ea40ddadb2c023b7b39e4abee284ab2045c37b2b562c508a73"
   license "MIT"
 
   depends_on :xcode => ["10.0", :build]
--- icomoongen.rb
+++ icomoongen.rb
@@ -1,8 +1,8 @@
 class Icomoongen < Formula
   desc "A command-line tool that generates type-safe code from IcoMoon font."
   homepage ""
-  url "https://github.com/tosaka07/IcoMoonGen/archive/refs/tags/0.0.1.tar.gz"
-  sha256 "ae449e95dc26fa81720a43b2b16f277d698e1998bff2d9d12354460e7dbc6d93"
+  url "https://github.com/tosaka07/IcoMoonGen/archive/refs/tags/0.0.2.tar.gz"
+  sha256 "471c2ff2bafbea451ec5d1e291144bdacab3be8db191a9041cd53b4f4609bcdd"
   license "MIT"
 
   depends_on :xcode => ["10.0", :build]

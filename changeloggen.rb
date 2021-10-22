736abf69e1b44d8ef36b217fc56660b4ce1a811c
tosaka07 <tosakaup@gmail.com>
2021-10-22 15:47:39 +0900

    Update

--- changeloggen.rb
+++ changeloggen.rb
@@ -1,8 +1,8 @@
 class Changeloggen < Formula
   desc "A command-line tool written in Swift that generates changelog"
   homepage "https://github.com/tosaka07/ChangelogGen"
-  url "https://github.com/tosaka07/ChangelogGen/archive/0.0.4.tar.gz"
-  sha256 "fbac8045e05439ea40ddadb2c023b7b39e4abee284ab2045c37b2b562c508a73"
+  url "https://github.com/tosaka07/ChangelogGen/archive/0.0.5.tar.gz"
+  sha256 "d0a21c55fc793fa6f3f8996fae46b01d986120500823913319631c825a48fd58"
   license "MIT"
 
   depends_on :xcode => ["10.0", :build]

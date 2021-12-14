7caef393255b5d1140b37502a1683e4236aeb709
tosaka07 <tosakaup@gmail.com>
2021-12-14 22:25:40 +0900

    Fix url of xtrail

--- xtrail.rb
+++ xtrail.rb
@@ -1,7 +1,7 @@
 class XTrail < Formula
   desc "A command line tool to easily install and browse Xcode templates."
   homepage "https://github.com/tosaka07/XTrail"
-  url "https://github.com/tosaka07/IcoMoonGen/archive/refs/tags/0.0.1.tar.gz"
+  url "https://github.com/tosaka07/XTrail/archive/refs/tags/0.0.1.tar.gz"
   sha256 "dac68943359db990e4b069be056de230ef2da9dcae69319d3acd121d37aaac17"
   license "MIT"
 

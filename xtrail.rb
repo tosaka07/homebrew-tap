d027ad03efb6dad7dad1a9c79c789d790b7b85be
tosaka07 <tosakaup@gmail.com>
2021-12-14 23:07:35 +0900

    Fix xtrail checksum 0.0.2

--- xtrail.rb
+++ xtrail.rb
@@ -2,7 +2,7 @@ class Xtrail < Formula
   desc "A command line tool to easily install and browse Xcode templates."
   homepage "https://github.com/tosaka07/XTrail"
   url "https://github.com/tosaka07/XTrail/archive/refs/tags/0.0.2.tar.gz"
-  sha256 "c4ad8e23579c79bc7696f4ea4d2d2ab138bacc679c2a0882f82339936c936a06"
+  sha256 "eebffc12fd1ecb9f06fef9b9a9ef6dc758eee3cfbefa4e97767fa378f2e27bd6"
   license "MIT"
 
   depends_on :xcode => ["11.0", :build]

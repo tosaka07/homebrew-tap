9c96ff07559dfe700c9a8aac7487de7d0e8d587b
tosaka07 <tosakaup@gmail.com>
2021-12-14 23:04:43 +0900

    Update xtrail to 0.0.2

--- xtrail.rb
+++ xtrail.rb
@@ -1,8 +1,8 @@
 class Xtrail < Formula
   desc "A command line tool to easily install and browse Xcode templates."
   homepage "https://github.com/tosaka07/XTrail"
-  url "https://github.com/tosaka07/XTrail/archive/refs/tags/0.0.1.tar.gz"
-  sha256 "dac68943359db990e4b069be056de230ef2da9dcae69319d3acd121d37aaac17"
+  url "https://github.com/tosaka07/XTrail/archive/refs/tags/0.0.2.tar.gz"
+  sha256 "c4ad8e23579c79bc7696f4ea4d2d2ab138bacc679c2a0882f82339936c936a06"
   license "MIT"
 
   depends_on :xcode => ["11.0", :build]

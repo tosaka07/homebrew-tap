a870b351bc11cef914899173d492e4a6df7f8f6c
tosaka07 <tosakaup@gmail.com>
2026-01-27 14:37:42 +0900

    Use prebuilt binaries for gwm

--- gwm.rb
+++ gwm.rb
@@ -1,14 +1,31 @@
 class Gwm < Formula
   desc "A fast and intuitive TUI application for managing git worktrees"
   homepage "https://github.com/tosaka07/gwm"
-  url "https://github.com/tosaka07/gwm/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "7de0b9b3a044f2ac9a0f662b3e84c7002399894d19553af30492f1d3e0e6bc81"
+  version "0.1.1"
   license "MIT"
 
-  depends_on "rust" => :build
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/tosaka07/gwm/releases/download/v0.1.1/gwm-aarch64-apple-darwin.tar.gz"
+      sha256 "18bc60357321c7e0b81b9b7401c19d777412039d4d3170e7e197725a3229af62"
+    else
+      url "https://github.com/tosaka07/gwm/releases/download/v0.1.1/gwm-x86_64-apple-darwin.tar.gz"
+      sha256 "4ab297a465ad0c9078135effaf6019cdcd1c31c039611b9822493de02b4db661"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/tosaka07/gwm/releases/download/v0.1.1/gwm-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "7a5a7537731e01dda482d59a7dee517a4134a15e814689943461d81b4e20d18c"
+    else
+      url "https://github.com/tosaka07/gwm/releases/download/v0.1.1/gwm-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "5cfa4f6d30fae6bff56f71da1cc0ad09a62b5f5520fc9677343b7b6e7566440f"
+    end
+  end
 
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "gwm"
   end
 
   test do
